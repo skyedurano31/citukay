b8852bbd22b8f16c3c8bfe3d1d4e8a69076af4cc
skyedurano <ashleeeskye@gmail.com>
2025-12-06 11:08:08 +0800

    styled login and register

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import './Login.css'
 
 const Login = () => {
   const [formData, setFormData] = useState({
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import './Register.css'
 
 const Register = () => {
   const [formData, setFormData] = useState({
